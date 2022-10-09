5726d00eec7441426fd4ccb0b1623c345ce010b7
Loc Nguyen <ngphloc@gmail.com>
2022-10-09 21:23:29 +0700

    Update classes.

--- 3_implementation/env.sh
+++ 3_implementation/env.sh
@@ -13,7 +13,7 @@ PATH=.:$JDK_HOME/bin:$JAVA_HOME/bin:$ANT_HOME/bin:$PATH:$JSI_OLD_PATH
 
 echo PATH=$PATH
 
-CLASSPATH=./hudup-core.jar:./hudup.jar:./sim.jar:./hudup-runtime-lib.jar:./sim-runtime-lib.jar:./jsi.jar:./bin:./lib/*:./working/lib/*:$EXTRA_CLASSPATH
+CLASSPATH=./hudup-core.jar:./hudup.jar:./sim.jar:./jsi.jar:./hudup-runtime-lib.jar:./sim-runtime-lib.jar:./jsi-runtime-lib.jar:./bin:./lib/*:./working/lib/*:$EXTRA_CLASSPATH
 
 echo CLASSPATH=$CLASSPATH
 
